738f3efc67757e1956908eeb885a00c2cce64e24
Izumitempest <lilice308@gmail.com>
2025-06-04 11:43:12 +0100

    Fixed "broadcast" TypeError

--- rat_server.py
+++ rat_server.py
@@ -120,4 +120,4 @@ def view_file(filename):
 if __name__ == "__main__":
     print("=== Yuno's RAT Server ===")
     print("A remote access tool by Yuno\n")
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
